f86fcb1e59b5ceeb7a821e3d8a125deb282da5f8
Alan Denison <alan79td@gmail.com>
2016-08-19 09:31:08 -0700

    add abillity to delete questions

--- app/components/pregunta-tile.js
+++ app/components/pregunta-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    delete(pregunta) {
+      if (confirm('¿está seguro quiere borrar esto?')) {
+        this.sendAction('destroyPregunta', pregunta);
+      }
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,4 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('pregunta');
+  },
+
+  actions: {
+    destroyPregunta(pregunta) {
+      pregunta.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
